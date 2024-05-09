9d9905e95adfb9161e606bd8157cd240be77f690
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-05-10 02:55:04 +0300

    add navbar with images

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,28 +1,7 @@
-'use client'
-
-import {
-	useParams,
-	usePathname,
-	useRouter,
-	useSearchParams,
-} from 'next/navigation'
-
 const PropertyPage = () => {
-	const router = useRouter()
-	const searchParams = useSearchParams()
-	const { id } = useParams()
-	const name = searchParams.get('name')
-	const pathname = usePathname()
-
 	console.log('Hello client')
 
-	return (
-		<div>
-			<button onClick={() => router.push('/')} className='bg-blue-500 p-2'>
-				Go home {pathname}
-			</button>
-		</div>
-	)
+	return <div>PropertyPage </div>
 }
 
 export default PropertyPage
